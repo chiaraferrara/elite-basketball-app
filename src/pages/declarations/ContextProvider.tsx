a7da8f07e03208fe2abb6c0299b470397f349df8
chiaraferrara <chiara.ferrara@stevejobs.academy>
2024-03-26 15:33:01 +0100

    feat: context provider and team detail page

---
+++ src/pages/declarations/ContextProvider.tsx
@@ -0,0 +1,98 @@
+import { ReactNode, createContext, useState } from "react";
+import { Player, TContext, Team } from "./declarations";
+
+export const Context = createContext<TContext>({
+  players: [],
+  setPlayers: () => {},
+  teams: [],
+  setTeams: () => {},
+  fetchPlayersAndTeams: async () => ({ teams: [] }),
+  fetchTeam: async () => ({}),
+});
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function ContextProvider({ children }: Props) {
+  const [players, setPlayers] = useState<Player[]>([]);
+  const [teams, setTeams] = useState<Team[]>([]);
+
+  //questo mi ritorna i team
+  const fetchTeams = async () => {
+    try {
+      const teamsRes = await fetch("/api/teams");
+      const teamsData = await teamsRes.json();
+      return teamsData;
+    } catch (error) {
+      console.error("Error:", error);
+      return [];
+    }
+  };
+
+  //questo mi ritorna i giocatori
+  const fetchPlayers = async () => {
+    try {
+      const playerRes = await fetch("/api/players");
+      const playerData = await playerRes.json();
+      return playerData;
+    } catch (error) {
+      console.error("Error", error);
+      return [];
+    }
+  };
+
+  const fetchPlayersAndTeams = async () => {
+    try {
+      const teamsData = await fetchTeams();
+      //   console.log("Teams data", teamsData);
+      const playerData = await fetchPlayers();
+
+      //aggiungo ai team i giocatori associati a quel team_id
+      teamsData.forEach((team: any) => {
+        team.players = playerData.filter(
+          (player: any) => player.team_id === team.id
+        );
+      });
+      //   console.log("Teams data with players", teamsData);
+      return { teams: teamsData };
+    } catch (error) {
+      console.error("Errore durante il recupero di giocatori e team:", error);
+      return { teams: [] };
+    }
+  };
+
+  //questo mi ritorna il team singolo con i giocatori associati
+  const fetchTeam = async (id: number) => {
+    try {
+      const playerData = await fetchPlayers();
+      const teamRes = await fetch(`/api/team/${id}`);
+      const teamData = await teamRes.json();
+
+      console.log("GIOCATORI", playerData);
+      teamData[0].players = playerData.filter(
+        (player: any) => player.team_id === id
+      );
+      // console.log("Questo è il teamData del Context", teamData);
+      return teamData;
+    } catch (error) {
+      console.error("Error:", error);
+      return null;
+    }
+  };
+
+  return (
+    <Context.Provider
+      value={{
+        players,
+        setPlayers,
+        teams,
+        setTeams,
+        fetchPlayersAndTeams,
+        fetchTeam,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { Context } from "./declarations/ContextProvider";
+import Link from "next/link";
 
 interface Team {
   id: number;
@@ -18,87 +20,26 @@ interface Player {
 export default function Home() {
   const [players, setPlayers] = useState<Player[]>([]);
   const [teams, setTeams] = useState<Team[]>([]);
-
-  const fetchPlayersAndTeams = async () => {
-    try {
-      const teamsRes = await fetch("/api/teams");
-      const teamsData = await teamsRes.json();
-
-      //devo inizializzare l'array players per ogni team
-
-      const teamsWithPlayerArray = teamsData.map((team: any) => ({
-        ...team,
-        players: [],
-      }));
-
-      setTeams(teamsWithPlayerArray);
-
-      const playerRes = await fetch("/api/players");
-      const playerData = await playerRes.json();
-
-      //questo mi serve per associare il nome della squadra al giocatore
-      playerData.forEach((player: any) => {
-        const foundTeam = teams.find(
-          (team: any) => team.team_id === player.team_id
-        );
-        if (foundTeam) {
-          player.team = foundTeam.name;
-          foundTeam.players.push(player);
-        }
-      });
-
-      setPlayers(playerData);
-
-      //aggiungo i giocatori ai team
-
-      teamsData.forEach((team: any) => {
-        const foundPlayers = playerData.filter(
-          (player: any) => player.team_id === team.team_id
-        );
-        if (foundPlayers) {
-          team.players = foundPlayers;
-          console.log("Trovati giocatori", foundPlayers);
-        }
-      });
-
-      setTeams(teamsData);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const { fetchPlayersAndTeams } = useContext(Context);
 
   useEffect(() => {
-    fetchPlayersAndTeams();
+    const fetchData = async () => {
+      const fetchedTeam = await fetchPlayersAndTeams();
+      setTeams(fetchedTeam.teams);
+    };
+
+    fetchData();
   }, []);
 
   return (
     <>
-      {/* <button
-        onClick={() => {
-          fetchPlayersAndTeams();
-        }}
-      >
-        Show Players and Teams
-      </button> */}
-
-      {/* VIEW PLAYERS */}
-
-      {/* {players.map((player: any) => (
-        <div key={player.id}>
-          <h1>{player.name}</h1>
-          <p>
-            Team:
-            <h3>{player.team_id}</h3>
-            <h1>{player.team}</h1>
-          </p>
-        </div>
-      ))}
-      <hr /> */}
-
+      <h1>Teams</h1>
       {teams.map((team: any) => (
         <div key={team.id}>
           <img src={team.logo} alt="team logo" />
           <h1>{team.name}</h1>
+          <Link href={`team/${team.team_id}`}>Go to team page</Link>
+          <br />
           Coach: <h3>{team.coach}</h3>
           {team.players.length > 0 && <p>Players:</p>}
           {team.players.map((player: any) => (
