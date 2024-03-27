f35283d55e3b82b65334db99cbc615976a7588ac
chiaraferrara <chiara.ferrara@stevejobs.academy>
2024-03-27 19:02:26 +0100

    feat: team page UI

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -9,11 +9,12 @@ export const globalStyles = css`
 `;
 
 export const Button = styled.button`
+    position: relative;
+    overflow: hidden;
     background-color: #f02b2b;
     color: black;
     font-weight: bold;
     height: 70px;
-    border-radius: 0.8em;
     font-size: 0.9rem;
     letter-spacing: 0.1rem;
     text-transform: uppercase;
@@ -22,40 +23,44 @@ export const Button = styled.button`
     margin-inline: 10px;
     border: none;
     cursor: pointer;
-    direction: rtl;
-    z-index: -1;
-    box-shadow:inset 2px 2px 2px 0px rgba(255,255,255,.5),
-    7px 7px 20px 0px rgba(0,0,0,.1),
-    4px 4px 5px 0px rgba(0,0,0,.1);
-   transition: all 0.3s ease;
+    z-index: 0;
+    transition: all 0.3s ease;
+
+    &:before {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 0;
+        background-color: #212322;
+        z-index: -1;
+        transition: height 0.3s ease;
+    }
+
+    &:hover:before {
+        height: 100%;
+    }
 
     &:hover {
-      color: #fff;
+        color: #fff;
     }
-    &:hover:after {
-      left: auto;
-      right: 0;
-      width: 100%;
-    }
-    &:active {
-      top: 2px;
-    }
+`;
 
-    &:hover {
-        background-color: #28292a;
-        margin-top: -2px;
-        color: white;
-        a {
-        color: black;
 
-        &:hover{
-          color: white;
-        }
-        }
-    }
+    export const Row = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+    align-items: center;
+    
+    `
+
+    export const PlayerName = styled.h3`
+    font-size: 0.5rem;
     `;
+
 export const Nav = styled.div`
-width: 100%;
     display: flex;
     flex-flow: row wrap;
     justify-content: center;
@@ -92,7 +97,7 @@ width: 100%;
 
     export const ContactWrapper = styled.div`
     display: flex;
-    flex-flow: column wrap;
+    flex-flow: row wrap;
     justify-content: center;
     align-items: flex-start
     padding: 1rem 2rem;
@@ -110,6 +115,23 @@ width: 100%;
     width: 80px;
     height: 80px;
     margin-right: 1rem;
+    object-fit: cover;
+    box-shadow: 0 2px 8px 0px rgb(0 0 0 / 25%);
+    `;
+
+    export const PointsDetail = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    background-color: #fff;
+    justify-content: center;
+    text-align: center;
+    padding: 2rem 3rem;
+    color: black;
+    font-size: 0.9rem;
+    font-weight: 600;
+    letter-spacing: 0.1rem;
+    margin-top: 1rem;
+    margin: 0 auto;
     `;
 
     export const RowWrap = styled.div`
@@ -125,4 +147,33 @@ width: 100%;
     width: 30px;
     height: 30px;
     margin-left: 1rem;
+    `;
+
+    export const Wrapper= styled.div`
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem 2rem;`
+
+    export const TeamWrapper = styled.div`
+    margin-top: 1rem;
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    background: rgb(33,35,34);
+    background: linear-gradient(180deg, rgba(33,35,34,1) 3%, rgba(255,255,255,1) 3%);background-color: #ffffff;
+    color: black;
+    font-size: 0.9rem;
+    font-weight: 400;
+    letter-spacing: 0.1rem;
+    text-align: center;
+
+    h1{
+      color: #black;
+      font-size: 1.5rem;
+    }
+    
     `;
