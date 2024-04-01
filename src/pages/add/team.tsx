017b5951d98cba3e690de6c2eb77355f245451b3
chiaraferrara <123738595+choorywey@users.noreply.github.com>
2024-04-01 20:32:44 +0200

    feat: forms UI and check on logged status

--- src/pages/add/team.tsx
+++ src/pages/add/team.tsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { CardForm, Input, PageButton, TeamGameColumn } from "@/styles/globals";
+import { useContext, useEffect, useState } from "react";
+import { Context } from "../declarations/ContextProvider";
 
 export default function Team() {
   const [teamName, setTeamName] = useState("");
@@ -7,6 +9,7 @@ export default function Team() {
   const [points_given, setPointsGiven] = useState(0);
   const [points_scored, setPointsScored] = useState(0);
   const [total_points, setTotalPoints] = useState(0);
+  const { setIsLogged, isLogged } = useContext(Context);
 
   const addTeamtoDB = async (event: any) => {
     if (!teamName || !coachName || !logoSrc) {
@@ -33,60 +36,87 @@ export default function Team() {
     console.log(data);
   };
 
+  useEffect(() => {
+    const logged = localStorage.getItem("isLogged");
+    if (logged) {
+      setIsLogged(true);
+    } else {
+      setIsLogged(false);
+    }
+  }, []);
+
+  if (!isLogged) {
+    return (
+      <TeamGameColumn>
+        <h1>Log in to add a team</h1>
+      </TeamGameColumn>
+    );
+  }
   return (
     <>
-      <div>
+      <TeamGameColumn>
         <h1>Add Team</h1>
-
-        <form
-          method="post"
-          onSubmit={(event) => {
-            event.preventDefault();
-            addTeamtoDB(event);
+        <CardForm
+          style={{
+            paddingBlock: "5%",
+            paddingInline: "9%",
+            textAlign: "center",
+            width: "fit-content",
           }}
         >
-          <div>
-            <label htmlFor="teamName">Team Name:</label>
-            <input
-              type="text"
-              id="teamName"
-              name="teamName"
-              value={teamName}
-              onChange={(event) => {
-                setTeamName(event.target.value);
-                console.log(teamName);
-              }}
-            />
-          </div>
-          <div>
-            <label>Coach:</label>
-            <input
-              type="text"
-              id="coach"
-              name="coach"
-              value={coachName}
-              onChange={(event) => {
-                setCoachName(event.target.value);
-                console.log(coachName);
-              }}
-            />
-          </div>
-          <div>
-            <label htmlFor="logo">Logo:</label>
-            <input
-              type="text"
-              id="logo"
-              name="logo"
-              value={logoSrc}
-              onChange={(event) => {
-                setLogoSrc(event.target.value);
-                console.log(logoSrc);
-              }}
-            />
-          </div>
-          <button type="submit">Add Team</button>
-        </form>
-      </div>
+          <form
+            method="post"
+            onSubmit={(event) => {
+              event.preventDefault();
+              addTeamtoDB(event);
+            }}
+          >
+            <div>
+              <legend>Team Name:</legend>
+              <Input
+                type="text"
+                id="teamName"
+                name="teamName"
+                placeholder="Team Name*"
+                value={teamName}
+                onChange={(event) => {
+                  setTeamName(event.target.value);
+                  console.log(teamName);
+                }}
+              />
+            </div>
+            <div>
+              <legend>Coach:</legend>
+              <Input
+                type="text"
+                id="coach"
+                name="coach"
+                placeholder="Coach Name*"
+                value={coachName}
+                onChange={(event) => {
+                  setCoachName(event.target.value);
+                  console.log(coachName);
+                }}
+              />
+            </div>
+            <div>
+              <legend>Logo:</legend>
+              <Input
+                type="text"
+                id="logo"
+                name="logo"
+                placeholder="Logo URL*"
+                value={logoSrc}
+                onChange={(event) => {
+                  setLogoSrc(event.target.value);
+                  console.log(logoSrc);
+                }}
+              />
+            </div>
+            <PageButton type="submit">Add Team</PageButton>
+          </form>
+        </CardForm>
+      </TeamGameColumn>
     </>
   );
 }
