8d6c16881760d66a2aad5f2d34049d0ab09c1103
Accia <stefanocammarata@outlook.it>
2024-04-03 12:50:53 +0200

    feat:fixed bug delete team and delete player

--- src/pages/api/player/[player_id].ts
+++ src/pages/api/player/[player_id].ts
@@ -1,26 +1,33 @@
 import { executeQuery } from "@/pages/mysqldb";
 
+export default async function handler(req: any, res: any) {
+  const { player_id } = req.query;
 
-export default async function handler(
-  req : any,
-  res : any
-) {
-    const { player_id } = req.query
+  //ottenimento di un player
+  if (req.method === "GET") {
+    const player = await executeQuery(
+      "SELECT * FROM players WHERE player_id = ?",
+      [player_id]
+    );
+    res.status(200).json(player);
+  }
 
-//ottenimento di un player
-    if (req.method === "GET") {
-      const player = await executeQuery(
-        'SELECT * FROM players WHERE player_id = ?', [player_id]
-      );
-      res.status(200).json(player);
-    }
-    
-//modifica del player
-    if (req.method === "PUT") {
-      const { picture, name, age, team_id , player_id } = req.body;
-      const player = await executeQuery(
-        'UPDATE players SET picture = ?, name = ?, age = ?, team_id = ? WHERE player_id = ?', [picture, name, age,  team_id, player_id]
-      );
-      res.status(200).json(player);
-    }
-  } 
+  //modifica del player
+  if (req.method === "PUT") {
+    const { picture, name, age, team_id, player_id } = req.body;
+    const player = await executeQuery(
+      "UPDATE players SET picture = ?, name = ?, age = ?, team_id = ? WHERE player_id = ?",
+      [picture, name, age, team_id, player_id]
+    );
+    res.status(200).json(player);
+  }
+
+  //eliminazione dei player
+  if (req.method === "DELETE") {
+    const player = await executeQuery(
+      "DELETE FROM players WHERE player_id = ?",
+      [player_id]
+    );
+    res.status(200).json(player);
+  }
+}
