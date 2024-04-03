4aab6a120018485ad39b58a991dd98eff13ea706
Accia <stefanocammarata@outlook.it>
2024-04-03 17:23:48 +0200

    feat: updated "Games" page's "delete" button

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -22,12 +22,50 @@ export default function Games() {
   const { games, setGames } = useContext(Context);
   const { update } = useContext(Context);
   const { isLogged } = useContext(Context);
+  const { updateLeaderboard, setUpdateLeaderboard } = useContext(Context);
 
-  useEffect(() => {
-    fetchGames().then(() => setLoading(false));
-    const recentGames = games.reverse().slice(0, 4);
-    setGames(recentGames);
-  }, [update, loading]);
+  const patchTeam = async (
+    team_id: any,
+    points_scored: number,
+    points_given: number,
+    total_points: number,
+    played_games: number,
+    sum: boolean
+  ) => {
+    const response = await fetch(`/api/team/${team_id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        points_scored: points_scored,
+        points_given: points_given,
+        total_points: total_points,
+        played_games: played_games,
+        sum: sum,
+      }),
+    });
+    const data = await response.json();
+  };
+
+  const updateTeamPoints = async (
+    team_id: any,
+    id_team1: any,
+    id_team2: any,
+    points_team1: any,
+    points_team2: any,
+    sum: boolean
+  ) => {
+    if (team_id == id_team1 && points_team1 > points_team2) {
+      patchTeam(id_team1, points_team1, points_team2, 2, 1, sum);
+    } else if (team_id == id_team2 && points_team2 > points_team1) {
+      patchTeam(id_team2, points_team2, points_team1, 2, 1, sum);
+    } else if (team_id == id_team1 && points_team1 < points_team2) {
+      patchTeam(id_team1, points_team1, points_team2, 0, 1, sum);
+    } else if (team_id == id_team2 && points_team2 < points_team1) {
+      patchTeam(id_team2, points_team2, points_team1, 0, 1, sum);
+    }
+  };
 
   const onClickDelete = async (game_id: any) => {
     const response = await fetch(`/api/game/${game_id}`, {
@@ -39,6 +77,12 @@ export default function Games() {
     const data = await response.json();
     console.log(data);
   };
+
+  useEffect(() => {
+    fetchGames().then(() => setLoading(false));
+    const recentGames = games.reverse().slice(0, 4);
+    setGames(recentGames);
+  }, [update, loading]);
 
   //metodo per formattare la data da "2024-03-13T23:00:00.000Z" a "13/03/2024"
   const formatDate = (dateString: any) => {
@@ -81,6 +125,27 @@ export default function Games() {
             {isLogged ? (
               <PageButton
                 onClick={() => {
+                  updateTeamPoints(
+                    game.id_team1,
+                    game.id_team1,
+                    game.id_team2,
+                    game.team1_points,
+                    game.team2_points,
+                    false
+                  )
+                    .then(() => {
+                      updateTeamPoints(
+                        game.id_team2,
+                        game.id_team1,
+                        game.id_team2,
+                        game.team1_points,
+                        game.team2_points,
+                        false
+                      );
+                    })
+                    .then(() => {
+                      setUpdateLeaderboard(!updateLeaderboard);
+                    });
                   onClickDelete(game.id_game);
                 }}
                 type="button"
