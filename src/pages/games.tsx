bec47be944fbfd418750c52e2b2d859d3de4fdfb
Accia <stefanocammarata@outlook.it>
2024-04-03 16:53:50 +0200

    feat: added "Games" page, edited index

---
+++ src/pages/games.tsx
@@ -0,0 +1,98 @@
+import Link from "next/link";
+import { Player, Team } from "./declarations/declarations";
+import { useContext, useEffect, useState } from "react";
+import { Context } from "./declarations/ContextProvider";
+import { TeamDiv, TeamImg, TeamRow } from "@/styles/globals";
+import {
+  Button,
+  DateRow,
+  GameWrapper,
+  GamesRow,
+  PageButton,
+  PlayerName,
+  PreviewPic,
+  Row,
+  TeamGameColumn,
+  TeamName,
+} from "@/styles/globals";
+
+export default function Games() {
+  const [loading, setLoading] = useState(true);
+  const { fetchGames } = useContext(Context);
+  const { games, setGames } = useContext(Context);
+  const { update } = useContext(Context);
+  const { isLogged } = useContext(Context);
+
+  useEffect(() => {
+    fetchGames().then(() => setLoading(false));
+    const recentGames = games.reverse().slice(0, 4);
+    setGames(recentGames);
+  }, [update, loading]);
+
+  const onClickDelete = async (game_id: any) => {
+    const response = await fetch(`/api/game/${game_id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      setLoading(true);
+    }
+    const data = await response.json();
+    console.log(data);
+  };
+
+  //metodo per formattare la data da "2024-03-13T23:00:00.000Z" a "13/03/2024"
+  const formatDate = (dateString: any) => {
+    const date = new Date(dateString);
+    //padStart serve per aggiungere uno 0 davanti ai numeri < 10
+    const day = date.getDate().toString().padStart(2, "0");
+    //i mesi partono da 0 quindi aggiungo 1
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
+
+  if (loading) return <GamesRow>Loading games...</GamesRow>;
+
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>League games</h1>
+      <GamesRow>
+        {games?.map((game: any) => (
+          <div key={game.id}>
+            <DateRow>{formatDate(game.date)}</DateRow>
+            <GameWrapper>
+              <TeamGameColumn>
+                <span>
+                  <PreviewPic src={game.home_team.logo} />
+                </span>
+                <TeamName>{game.home_team.name}</TeamName>
+                <p> {game.team1_points}</p>
+              </TeamGameColumn>
+              <br />
+              <p>vs</p>{" "}
+              <TeamGameColumn>
+                <span>
+                  <PreviewPic src={game.away_team.logo} />
+                </span>
+                <TeamName>{game.away_team.name}</TeamName>
+                <p> {game.team2_points}</p>{" "}
+              </TeamGameColumn>
+            </GameWrapper>
+            {isLogged ? (
+              <PageButton
+                onClick={() => {
+                  onClickDelete(game.id_game);
+                }}
+                type="button"
+              >
+                delete
+              </PageButton>
+            ) : (
+              <></>
+            )}
+          </div>
+        ))}
+      </GamesRow>
+    </>
+  );
+}
