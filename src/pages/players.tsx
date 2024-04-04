863b63dade9f032fa090f46c4d8f54e548baebb4
chiaraferrara <123738595+choorywey@users.noreply.github.com>
2024-04-04 16:40:18 +0200

    feat: display message if team, players, games haven't been added yet

--- src/pages/players.tsx
+++ src/pages/players.tsx
@@ -1,7 +1,15 @@
 import { useContext, useEffect, useState } from "react";
 import { Context } from "./declarations/ContextProvider";
-import { PageButton, PreviewPic, Row, RowWrap } from "@/styles/globals";
-import { loadBindings } from "next/dist/build/swc";
+import {
+  CardForm,
+  PageButton,
+  PreviewPic,
+  Row,
+  RowWrap,
+} from "@/styles/globals";
+
+import sadFace from "../assets/sad.svg";
+import basketBall from "../assets/basketball.svg";
 
 export default function PlayersPage() {
   const { fetchPlayers, players, setPlayers } = useContext(Context);
@@ -21,32 +29,65 @@ export default function PlayersPage() {
 
   return (
     <div>
-      <h1>Players</h1>
-      <Row
-        style={{
-          justifyContent: "space-around",
-          alignItems: "center",
-          flexWrap: "wrap",
-          textAlign: "center",
-        }}
-      >
-        {players.map((player) => (
-          <div key={player.player_id}>
-            <PreviewPic src={player.picture} alt="player pic" />
-            <h2>{player.name}</h2>
-            <p>Age : {player.age}</p>
-            <PageButton
-              onClick={() => {
-                onClickDelete(player.player_id).then(() => {
-                  setLoading(!loading);
-                });
-              }}
-            >
-              delete
-            </PageButton>
-          </div>
-        ))}
-      </Row>
+      <h1 style={{ textAlign: "center" }}>Players</h1>
+      {players.length > 0 ? (
+        <>
+          <Row
+            style={{
+              justifyContent: "space-around",
+              alignItems: "center",
+              flexWrap: "wrap",
+              textAlign: "center",
+            }}
+          >
+            {players.map((player) => (
+              <div key={player.player_id}>
+                <PreviewPic src={player.picture} alt="player pic" />
+                <h2>{player.name}</h2>
+                <p>Age : {player.age}</p>
+                <PageButton
+                  onClick={() => {
+                    onClickDelete(player.player_id).then(() => {
+                      setLoading(!loading);
+                    });
+                  }}
+                >
+                  delete
+                </PageButton>
+              </div>
+            ))}
+          </Row>
+        </>
+      ) : (
+        <>
+          <>
+            <CardForm>
+              <div
+                style={{
+                  display: "flex",
+                  flexFlow: "column",
+                  textAlign: "center",
+                }}
+              >
+                <div>
+                  {" "}
+                  <img
+                    style={{ width: "40px" }}
+                    src={sadFace.src}
+                    alt="sad face"
+                  />
+                  <img
+                    style={{ width: "40px" }}
+                    src={basketBall.src}
+                    alt="basketball"
+                  />
+                </div>
+                <p>Unfortunately there's no players to display.</p>
+              </div>
+            </CardForm>
+          </>
+        </>
+      )}
     </div>
   );
 }
--- src/pages/teams.tsx
+++ src/pages/teams.tsx
@@ -2,7 +2,9 @@ import Link from "next/link";
 import { Player, Team } from "./declarations/declarations";
 import { useContext, useEffect, useState } from "react";
 import { Context } from "./declarations/ContextProvider";
-import { TeamDiv, TeamImg, TeamRow } from "@/styles/globals";
+import { CardForm, TeamDiv, TeamImg, TeamRow } from "@/styles/globals";
+import sadFace from "../assets/sad.svg";
+import basketBall from "../assets/basketball.svg";
 
 export default function Teams() {
   const [players, setPlayers] = useState<Player[]>([]);
@@ -21,20 +23,43 @@ export default function Teams() {
   return (
     <>
       <h1 style={{ textAlign: "center" }}>Teams</h1>
-      <TeamRow>
-        {teams.map((team: any) => (
-          <TeamDiv key={team.id}>
-            <Link href={`team/${team.team_id}`}>
-              <TeamImg src={team.logo} alt="team logo" />
-            </Link>
+      {teams.length > 0 ? (
+        <TeamRow>
+          {teams.map((team: any) => (
+            <TeamDiv key={team.id}>
+              <Link href={`team/${team.team_id}`}>
+                <TeamImg src={team.logo} alt="team logo" />
+              </Link>
 
-            <Link href={`team/${team.team_id}`}>
-              <h1>{team.name}</h1>
-            </Link>
-            <br />
-          </TeamDiv>
-        ))}
-      </TeamRow>
+              <Link href={`team/${team.team_id}`}>
+                <h1>{team.name}</h1>
+              </Link>
+              <br />
+            </TeamDiv>
+          ))}
+        </TeamRow>
+      ) : (
+        <CardForm>
+          <div
+            style={{
+              display: "flex",
+              flexFlow: "column",
+              textAlign: "center",
+            }}
+          >
+            <div>
+              {" "}
+              <img style={{ width: "40px" }} src={sadFace.src} alt="sad face" />
+              <img
+                style={{ width: "40px" }}
+                src={basketBall.src}
+                alt="basketball"
+              />
+            </div>
+            <p>Unfortunately there's no teams to display.</p>
+          </div>
+        </CardForm>
+      )}
     </>
   );
 }
